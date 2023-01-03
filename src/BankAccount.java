dbd9cbe69ec4f543fad5e47fe65cf61e519f06da
nurislam09 <akinatorxxl@gmail.com>
2023-01-03 15:12:26 +0600

    task b finish

---
+++ src/BankAccount.java
@@ -0,0 +1,16 @@
+public class BankAccount {
+    private double amount;
+
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public void deposit(double sum){
+
+    }
+
+    public void withDraw(int sum){
+
+    }
+}
